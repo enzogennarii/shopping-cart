e19e5d5845d5222295a6c27412a589f1c652cc93
Enzo Gennari <enzo.gennari02@gmail.com>
2023-03-23 17:41:22 -0300

    Conclui parte dos requisitos 1 e 2

--- src/tests/fetchProductsList.test.js
+++ src/tests/fetchProductsList.test.js
@@ -5,17 +5,26 @@ import computadorSearch from './mocks/search';
 // implemente seus testes aqui
 describe('Teste a função fetchProductsList', () => {
   it('fetchProductsList é uma função', () => {
-    
+    expect(typeof fetchProductsList).toBe('function');
   });
 
   it('fetch é chamado ao executar fetchProductsList', () => {
-
+    fetchProductsList('computador');
+    expect(fetch).toBeCalled();
   });
 
   it('fetch é chamado com o endpoint correto ao executar fetchProductsList', () => {
-
+    fetchProductsList('computador');
+    expect(fetch).toHaveBeenCalledWith('https://api.mercadolibre.com/sites/MLB/search?q=computador');
   });
 
-  // it('...', () => {
+  it('o retorno da função com o argumento "computador" é uma estrutura de dados igual à computadorSearch', () => {
+    const returned = fetchProductsList('computador');
+    expect(typeof returned).toBe(typeof computadorSearch);
+  });
+
+  // it('o retorno da função sem argumento é uma mensagem de erro "Termo de busca não informado"', () => {
+  //   expect(() => { fetchProductsList() }).toThrowError();
+  //   expect(() => { fetchProductsList() }).toThrow("Termo de busca não informado");
   // });
 });
