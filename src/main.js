0720384a78ddd3210b4112a80d70d629a4fa85c5
Enzo Gennari <enzo.gennari02@gmail.com>
2023-03-23 18:14:09 -0300

    Conclui requisito 3

--- src/main.js
+++ src/main.js
@@ -1,4 +1,18 @@
 import { searchCep } from './helpers/cepFunctions';
+import { fetchProductsList } from './helpers/fetchFunctions';
+import { createProductElement } from './helpers/shopFunctions';
 import './style.css';
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
+
+const sectionProducts = document.querySelector('.products');
+
+const showProductSearched = async (product) => {
+  const products = await fetchProductsList(product);
+  products.forEach((item) => {
+    const createItemProduct = createProductElement(item);
+    sectionProducts.appendChild(createItemProduct);
+  });
+};
+
+showProductSearched('computador');
