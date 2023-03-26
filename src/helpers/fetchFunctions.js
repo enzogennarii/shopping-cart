4f313f90a843b60ec6741883838d239451c1b80f
Enzo Gennari <enzo.gennari02@gmail.com>
2023-03-26 19:59:41 -0300

    Cria função loading e removeLoading

--- src/helpers/fetchFunctions.js
+++ src/helpers/fetchFunctions.js
@@ -1,18 +1,31 @@
-export const fetchProduct = async (id) => {
-  if (!id) { throw new Error('ID não informado'); }
-
-  const response = await fetch(`https://api.mercadolibre.com/items/${id}`);
-  const data = await response.json();
-  console.log(data);
-  return data;
+const createErrorMessage = () => {
+  const sectionProducts = document.querySelector('.products');
+  const errorMessage = document.createElement('p');
+  errorMessage.innerText = 'Algum erro ocorreu, recarregue a página e tente novamente';
+  errorMessage.classList.add('error');
+  sectionProducts.appendChild(errorMessage);
 };
 
-fetchProduct('MLB1405519561');
+export const fetchProduct = async (id) => {
+  if (!id) { throw new Error('ID não informado'); }
+  try {
+    const response = await fetch(`https://api.mercadolibre.com/items/${id}`);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    createErrorMessage();
+    console.log('entrou no erro');
+  }
+};
 
 export const fetchProductsList = async (product) => {
   if (!product) { throw new Error('Termo de busca não informado'); }
-
-  const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${product}`);
-  const data = await response.json();
-  return data.results;
+  try {
+    const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${product}`);
+    const data = await response.json();
+    return data.results;
+  } catch (error) {
+    createErrorMessage();
+    console.log('entrou no erro');
+  }
 };
--- src/main.js
+++ src/main.js
@@ -4,10 +4,23 @@ import { createProductElement } from './helpers/shopFunctions';
 import './style.css';
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
-
 const sectionProducts = document.querySelector('.products');
 
+const loading = () => {
+  const loadingEl = document.createElement('p');
+  loadingEl.innerText = 'carregando...';
+  loadingEl.classList.add('loading');
+  sectionProducts.appendChild(loadingEl);
+};
+
+const removeLoading = () => {
+  const loadingEl = document.querySelector('.loading');
+  loadingEl.remove();
+};
+
 const showProductSearched = async (product) => {
+  removeLoading();
+  if (!product) { product = 'computador'; }
   const products = await fetchProductsList(product);
   products.forEach((item) => {
     const createItemProduct = createProductElement(item);
@@ -15,4 +28,7 @@ const showProductSearched = async (product) => {
   });
 };
 
-showProductSearched('computador');
+window.onload = () => {
+  loading();
+  showProductSearched();
+};
