3dd79b7b61e2a83cb62bbd0c3bca70e0aa2b14fa
Enzo Gennari <enzo.gennari02@gmail.com>
2023-03-26 22:13:06 -0300

    Conclui todos os requisitos de 1 a 7 e implementa o evento de salvar id no local storage do botão de adicionar ao carrinho

--- src/helpers/shopFunctions.js
+++ src/helpers/shopFunctions.js
@@ -1,4 +1,4 @@
-import { removeCartID } from './cartFunctions';
+import { removeCartID, saveCartID } from './cartFunctions';
 
 // Esses comentários que estão antes de cada uma das funções são chamados de JSdoc,
 // experimente passar o mouse sobre o nome das funções e verá que elas possuem descrições!
@@ -121,6 +121,9 @@ export const createProductElement = ({ id, title, thumbnail, price }) => {
     'product__add',
     'Adicionar ao carrinho!',
   );
+  cartButton.addEventListener('click', () => {
+    saveCartID(id);
+  });
   section.appendChild(cartButton);
 
   return section;
