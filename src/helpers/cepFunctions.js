ffbf669beb3e10c6e0dd1036ed1339bbd7a24cbe
Enzo Gennari <enzo.gennari02@gmail.com>
2023-07-31 13:18:03 -0300

    Requisito 11

--- src/helpers/cepFunctions.js
+++ src/helpers/cepFunctions.js
@@ -1,7 +1,30 @@
-export const getAddress = () => {
-  // seu código aqui
+export const getAddress = async (cep) => {
+  const promise1 = await fetch(`https://cep.awesomeapi.com.br/json/${cep}`)
+    .then((res) => res.json()).then((data) => data);
+
+  const promise2 = await fetch(`https://brasilapi.com.br/api/cep/v2/${cep}`)
+    .then((res) => res.json()).then((data) => data);
+
+  const address = Promise.any([promise1, promise2]);
+  return address;
 };
 
-export const searchCep = () => {
-  // seu código aqui
+export const searchCep = async () => {
+  const cep = document.querySelector('.cep-input').value || '0';
+  const span = document.querySelector('.cart__address');
+
+  const addressObj = await getAddress(cep);
+
+  if (addressObj.code) {
+    span.innerText = 'CEP não encontrado';
+    return;
+  }
+
+  if (addressObj.ddd) {
+    const { address, district, city, state } = addressObj;
+    span.innerText = `${address} - ${district} - ${city} - ${state}`;
+  } else {
+    const { street, neighborhood, city, state } = addressObj;
+    span.innerText = `${street} - ${neighborhood} - ${city} - ${state}`;
+  }
 };
